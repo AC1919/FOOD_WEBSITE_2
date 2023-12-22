3619359d04cb7e13e43fe52fb14bbf7f9cfd2847
AC1919 <144209630+AC1919@users.noreply.github.com>
2023-12-22 18:10:55 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style3.css">
+    <title>About Us</title>
+</head>
+<body>
+    <center>
+    <h1 style="color:white; font-size:24px">About Us</h1>
+    </center>
+     <p style="color:white;">Optio numquam provident nisi deleniti similique quisquam aspernatur perferendis, architecto eveniet cupiditate, placeat assumenda quidem explicabo obcaecati adipisci ad fugit.
+    Quisquam sit ipsam ea est quis aspernatur aliquam sed dolore delectus corrupti libero fuga ut voluptate, adipisci perferendis. Fuga eaque obcaecati est adipisci earum asperiores culpa eius qui sequi alias.
+    Repudiandae, a sed dolore aspernatur temporibus quae cupiditate tempore corrupti atque iure accusantium repellendus asperiores ea! Incidunt, laborum eligendi placeat asperiores explicabo adipisci quibusdam iste, ducimus nostrum accusamus quas alias?
+    Perferendis animi cumque debitis aut consequuntur cum eos, eum possimus modi quos mollitia quae culpa beatae explicabo reprehenderit esse voluptatum aliquam necessitatibus excepturi dignissimos sed magni voluptate corporis. Rerum, possimus.
+    Exercitationem, sequi error sed et autem suscipit quis porro illo, natus repudiandae consequatur accusamus excepturi ea? Labore nisi voluptatem voluptatibus possimus sed eaque aliquid corporis atque, ut, officiis molestiae quam!
+    Maiores ipsa iusto vero suscipit quos, saepe, voluptas, voluptatibus ab repellat recusandae molestiae voluptatum blanditiis. Ratione laborum aliquam maiores? Accusamus dignissimos, ad ab perspiciatis eligendi tempore quisquam modi possimus perferendis.
+    Earum cumque labore magni repudiandae delectus mollitia nisi corrupti dignissimos voluptatibus alias laudantium fugit ipsa nesciunt hic vero ducimus repellendus veniam, at asperiores omnis. Nam, nulla tempore. Distinctio, consectetur harum.
+    Assumenda pariatur sed a dignissimos officia earum laboriosam fugit adipisci, nostrum molestiae numquam labore et placeat consequuntur aliquid quaerat inventore quo consequatur eum. Aut rem odit tenetur aliquid, architecto natus.
+    Ullam qui eos delectus, sed autem, dolores facere alias aperiam aliquam molestias, pariatur ea tenetur ab repellendus iusto explicabo. Dolorum fuga tempora nihil accusamus illo cum saepe harum recusandae minima.
+    Minus fugit aperiam, autem facere doloremque aut suscipit vitae explicabo dolorum ipsam quis alias voluptatem ratione, harum error sed voluptatum temporibus molestias tenetur obcaecati debitis laudantium ab nam maxime! Ullam.
+    Expedita, ipsam recusandae aliquam unde nobis tenetur repellat mollitia voluptas veniam, blanditiis quasi nam rerum similique odio sapiente. Quidem quo deleniti veniam necessitatibus delectus sunt inventore saepe tenetur repellendus fuga.
+    Sit, odit et asperiores sapiente tempore architecto quam explicabo hic dolore unde debitis! Quidem, provident voluptate inventore saepe explicabo earum beatae illo? Quis reiciendis mollitia repudiandae iste. Repudiandae, voluptatem doloremque.
+    Tenetur, esse sunt! Rem cumque tempore, nostrum commodi ab et dolore sint sed, voluptate accusantium dolores, optio porro consequatur repudiandae explicabo iste nesciunt deleniti non neque vel sequi eveniet. Iste.
+    Modi dolores, facere hic facilis at cum illo, nobis saepe sapiente ab perspiciatis minus quisquam, sit quo laborum corporis! Iste blanditiis, error tempore sed est expedita explicabo aliquam dicta? Fugiat?
+    Sed vel fugiat accusantium aliquam pariatur est at corporis odio maxime ratione. Voluptatum voluptatibus magnam dolorum dolores error ex, ut deserunt numquam culpa, harum qui reprehenderit quisquam, fugiat molestias itaque?
+    Beatae incidunt aut voluptatem culpa enim vitae consequuntur aliquid ex! Deleniti, a in atque quaerat, earum reprehenderit ipsum aperiam molestias maxime iure explicabo libero! Et provident blanditiis dolorum nobis ratione.
+    Ad officia qui ducimus consectetur illum debitis, praesentium delectus magni amet, animi nobis corporis autem? Error omnis repellendus optio, laborum dolor beatae temporibus dolorum rerum reprehenderit minus excepturi iure nesciunt?
+    Dolor iste totam et sapiente doloremque, amet aliquid vitae ipsam ea repudiandae quisquam aliquam laborum fuga consequuntur magnam harum obcaecati? Aliquam hic assumenda repellendus possimus cumque, accusantium voluptatum nam fuga.
+    Recusandae, voluptatibus veniam in vitae non fugit eligendi, voluptate obcaecati accusamus, repellat dignissimos? Dicta et modi, ratione id, ullam unde quam placeat at inventore, possimus molestiae consequatur adipisci illo nam!</p>
+   <br>
+  <h1 style="color:white;"> CONTACT US FOR JOB OPPORTUNITIES</h1>
+    <table>
+   <form>
+      <tr>
+	
+	<td style="color:white;"><label for="fname">Full name:</label></td><br>
+  	<td style="color:white;"><input type="text" id="fname" name="fname"></td><br>
+      </tr>
+       <tr>
+ 	 <td style="color:white;"><label for="lname">Contact Number:</label></td><br>
+  	<td style="color:white;"><input type="text" id="lname" name="lname"></td><br>
+      </tr>
+      <tr>
+	<td style="color:white;"><button type="submit" name="submit">Submit</button>
+     </tr>
+</body>
+</html>
